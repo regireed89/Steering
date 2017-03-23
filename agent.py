274c26d86ef84497118370c3ebae7a97359c3ea3
regireed89 <regireed89@gmail.com>
2017-03-23 14:37:51 -0500

    vector math

---
+++ agent.py
@@ -0,0 +1,12 @@
+'''Agent class'''
+
+class Agent(object):
+    '''Agent'''
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.position = (self.posx, self.posy)
+        self.velocity = (self.x, self.y)
+        self.heading = 
+
---
+++ vectormath.py
@@ -0,0 +1,15 @@
+'''vector math library'''
+import math
+
+
+def magnitude(vector):
+    '''magnitude'''
+    return math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]))
+
+def normalize(vector):
+    '''normalize'''
+    return (vector[0]/magnitude(vector), vector[1]/magnitude(vector))
+
+def distance(vector1, vector2):
+    '''distance'''
+    return (vector1[0] - vector2[1], vector1[1] - vector2[1])
